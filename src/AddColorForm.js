29e95af61e103d56e12b1c60ae5bb0de8c72ad7c
Elizaveta <isabethia7@gmail.com>
2023-06-02 11:47:48 +0300

    final color organizer

--- src/AddColorForm.js
+++ src/AddColorForm.js
@@ -1,22 +1,28 @@
-import React, { useRef } from "react";
+import React from "react";
+import { useInput } from "./hooks";
+import { useColors } from "./ColorProvider";
 
-export default function AddColorForm({ onNewColor = f => f }) {
-    const txtTitle = useRef();
-    const hexColor = useRef();
+export default function AddColorForm() {
+    const [titleProps, resetTitle] = useInput("");
+    const [colorProps, resetColor] = useInput("#000000");
+    const { addColor } = useColors();
 
     const submit = e => {
         e.preventDefault();
-        const title = txtTitle.current.value;
-        const color = hexColor.current.value;
-        onNewColor(title, color);
-        txtTitle.current.value = "";
-        hexColor.current.value = "";
+        addColor(titleProps.value, colorProps.value);
+        resetTitle();
+        resetColor();
     };
 
     return (
         <form onSubmit={submit}>
-            <input ref={txtTitle} type="text" placeholder="color title..." required />
-            <input ref={hexColor} type="color" required />
+            <input
+                {...titleProps}
+                type="text"
+                placeholder="color title..."
+                required
+            />
+            <input {...colorProps} type="color" required />
             <button>ADD</button>
         </form>
     );
